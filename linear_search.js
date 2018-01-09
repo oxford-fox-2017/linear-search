317cb66d8efb83e8999580f4082b630ed242d610
aldiws <syldie.aldiwijaya@gmail.com>
2018-01-09 11:09:04 +0700

    release-1

--- linear_search.js
+++ linear_search.js
@@ -21,7 +21,14 @@ console.log(linearSearch(9, random_numbers));
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-  //write your code here  
+  //write your code here
+  let result = []
+  for (let i = 0; i < values.length; i++) {
+    if (target === values[i]) {
+      result.push(i)
+    }
+  }
+  return resultgit 
 }
 
 let banana_arr = "banana".split("");
