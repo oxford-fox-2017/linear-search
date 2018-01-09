ff3f04fb22ea9baab79dfd80fa68cb0fba06180c
ferdiereinaldo <ferdiereinaldo@gmail.com>
2018-01-09 15:00:42 +0700

    Release 0 & 1

--- linear_search.js
+++ linear_search.js
@@ -13,10 +13,44 @@ console.log(linearSearch(18, random_numbers));
 console.log(linearSearch(9, random_numbers));
 // -1
 
+'use strict'
+
+// Release 0
+console.log("Linear Search")
+let linearSearch = (target, values) => {
+  //write your code here
+
+  for(var i = 0; i < values.length; i++) {
+    if(values[i] === target) {
+      return values.indexOf(target);
+    }
+  }
+  return -1;
+}
+
+let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
+
+console.log(linearSearch(72, random_numbers));
+// 2
+console.log(linearSearch(9, random_numbers));
+// -1
+
+/*===============================================================*/
+
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  var arr = [];
+  var newStr = values;
+  
+  for(var i = 0; i < values.length; i++) {
+    if(newStr[i] === target) {
+      arr.push(i);
+    }
+  }
+  
+  return arr;
 }
 
 let banana_arr = "banana".split("");
@@ -24,7 +58,7 @@ let banana_arr = "banana".split("");
 console.log(globalLinearSearch("a", banana_arr));
 // [ 1, 3, 5 ]
 
-module.exports = {
-  linearSearch,
-  globalLinearSearch
-}
+// module.exports = {
+//   linearSearch,
+//   globalLinearSearch
+// }
