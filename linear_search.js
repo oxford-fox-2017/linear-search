a879c0fbd0f75027f6c886ec33ad0db35de39380
adhiartaputra <adhiartaprabataputra@gmail.com>
2018-01-09 15:30:23 +0700

    Release 0

--- linear_search.js
+++ linear_search.js
@@ -3,10 +3,16 @@
 // Release 0
 console.log("Linear Search")
 let linearSearch = (target, values) => {
-  //write your code here
+    //write your code here
+    for (let i = 0; i < values.length; i++) {
+        if (target === values[i]) {
+            return i
+        }
+    }
+    return -1
 }
 
-let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
+let random_numbers = [6, 29, 18, 2, 72, 19, 18, 10, 37];
 
 console.log(linearSearch(18, random_numbers));
 // 2
@@ -16,7 +22,8 @@ console.log(linearSearch(9, random_numbers));
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-  //write your code here
+    //write your code here
+
 }
 
 let banana_arr = "banana".split("");
@@ -25,6 +32,6 @@ console.log(globalLinearSearch("a", banana_arr));
 // [ 1, 3, 5 ]
 
 module.exports = {
-  linearSearch,
-  globalLinearSearch
+    linearSearch,
+    globalLinearSearch
 }
