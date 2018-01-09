354fe3b6a35388c264d5d7d74461e4046e6f057d
hacktiv8 <hacktiv8@H8JKT006.local>
2018-01-09 15:21:38 +0700

    linear_search release 1

--- linear_search.js
+++ linear_search.js
@@ -1,30 +1,37 @@
-'use strict'
+//release 0
+//PSEUDOCODE
+//lakukan pengulangan untuk pengecekan kesamaan nilai yang di cari dengan nilai yang ada di dalam Array
+//tampilkan posisi dalam array pada saat ditemukan kesamaan
+//jika tidak ditemukan kesamaan tulis -1
 
-// Release 0
-console.log("Linear Search")
-let linearSearch = (target, values) => {
-  //write your code here
+function linierSearch(angkaPembanding,random_numbers){
+	for(let i = 0;i<random_numbers.length;i++){
+		if(random_numbers[i] == angkaPembanding){
+			return i;
+		}
+	}
+	return -1;
 }
 
-let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
+console.log(linierSearch(18,[6,29,18,2,72,19,18,10,37]))//2;
+console.log(linierSearch(9,[6,29,18,2,72,19,18,10,37]))//-1;
 
-console.log(linearSearch(18, random_numbers));
-// 2
-console.log(linearSearch(9, random_numbers));
-// -1
+//release 1 
+//PSEUDOCODE
+//buat varialbel tampung
+//lakukan pengulangan untuk pengecekan kesamaan nilai yang di cari dengan nilai yang ada di dalam Array
+//push posisi dalam array pada saat ditemukan kesamaan
+//tampilkan tampung
 
-// Release 1
-console.log("Global Linear Search")
-let globalLinearSearch = (target, values) => {
-  //write your code here
+function globalLinierSearch(hurufDicari,kata){
+	let posisiAngkaDicari = [];
+	for(let i = 0;i<kata.length;i++){
+		if(kata[i] === hurufDicari){
+			posisiAngkaDicari[posisiAngkaDicari.length]=i;
+		}
+	}
+	return posisiAngkaDicari;
 }
 
-let banana_arr = "banana".split("");
-// ["b", "a", "n", "a", "n", "a"]
-console.log(globalLinearSearch("a", banana_arr));
-// [ 1, 3, 5 ]
-
-module.exports = {
-  linearSearch,
-  globalLinearSearch
-}
+console.log(globalLinierSearch('a','banana'));
+console.log(globalLinierSearch('c','cucakcabe'));
