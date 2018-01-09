3b45ffc74c52dd94f3439f99c25234503184fb6e
dorman99 <hafizh.abdillahhh@gmail.com>
2018-01-09 13:09:11 +0700

    done

--- linear_search.js
+++ linear_search.js
@@ -3,7 +3,15 @@
 // Release 0
 console.log("Linear Search")
 let linearSearch = (target, values) => {
-  //write your code here
+  for (const value in values) {
+    if (values.hasOwnProperty(value)) {
+      const element = values[value];
+      if(element == target){
+        return value
+      }
+    }
+  }
+  return -1
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -13,18 +21,27 @@ console.log(linearSearch(18, random_numbers));
 console.log(linearSearch(9, random_numbers));
 // -1
 
-// Release 1
+// // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-  //write your code here
+  let arr = []
+  for (const value in values) {
+    if (values.hasOwnProperty(value)) {
+      const element = values[value];
+      if(element == target){
+        arr.push(value)
+      }
+    }
+  }
+  return arr
 }
 
 let banana_arr = "banana".split("");
-// ["b", "a", "n", "a", "n", "a"]
+// // ["b", "a", "n", "a", "n", "a"]
 console.log(globalLinearSearch("a", banana_arr));
-// [ 1, 3, 5 ]
+// // [ 1, 3, 5 ]
 
-module.exports = {
-  linearSearch,
-  globalLinearSearch
-}
+// module.exports = {
+//   linearSearch,
+//   globalLinearSearch
+// }
